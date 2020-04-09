c2ccbd92f558179e7460a575a22d9ad5b83a6d77
MatiasBoldini <matiboldini2000@gmail.com>
2020-04-09 17:08:15 -0300

    ejercicio 8 con posible bug

---
+++ Punteros tps/tp8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+char cadena[50];
+int vocales (char*);
+int c, cont=0;
+
+int main(){
+    cout<<"Ingrese cantidad de caracteres pertenecientes a la cadena: "<<endl;
+    vocales(cadena);
+    c=cont;
+    cout<<"Cantidad de vocales minusculas; "<<c<<endl;
+}
+
+int vocales(char*p){
+    cin.getline(cadena, 50);
+    int lon=strlen(p);
+    cout<<"longitud del puntero "<<lon<<endl;
+    for (int i=0; i<lon; i++){
+        if ((*(p+i)=='a') || (*(p+i)=='e') || (*(p+i)=='i') || (*(p+i)=='o') || (*(p+i)=='u')){
+            cont ++;
+        return cont;
+        }
+    }
+}
